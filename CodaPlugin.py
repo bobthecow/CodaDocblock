c8b6c4cafe2290df34695db0d8920fd38266b0d9
Justin Hileman <justin@justinhileman.info>
2010-02-18 01:08:32 -0500

    customize classes, documentation for Docblock plugin.

--- CodaPlugin.py
+++ CodaPlugin.py
@@ -1,27 +1,7 @@
 '''
-Coda Plugin Skeleton
+Docblock
 
-A poorly named framework for writing Cocoa plugins for Coda using Python.
-
-Copyright (c) 2009 Ian Beck
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+Copyright (c) 2010 Justin Hileman
 '''
 
 import sys
@@ -33,14 +13,14 @@ import objc
 NSObject = objc.lookUpClass('NSObject')
 CodaPlugIn = objc.protocolNamed('CodaPlugIn')
 
-class CodaPluginSkeleton(NSObject, CodaPlugIn):
+class Docblock(NSObject, CodaPlugIn):
     '''
     Initializes the menu items and is responsible for directing
     actions to the appropriate class
     '''
     
 	# AT A MINIMUM you must change this line:
-    plugin_name = 'Coda Plugin Skeleton'
+    plugin_name = 'Docblock'
     
     def initWithPlugInController_bundle_(self, controller, bundle):
         '''Required method; run when the plugin is loaded'''
