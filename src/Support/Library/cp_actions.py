356e403b3cfca2ff4c50507030c8ed02e27ff125
Justin Hileman <justin@justinhileman.info>
2010-02-26 17:31:48 -0500

    Changes from manipulate lines.

--- src/Support/Library/cp_actions.py
+++ src/Support/Library/cp_actions.py
@@ -70,7 +70,7 @@ def get_line_after(context, range = None):
     return get_selection(context, new_range(start, end - start))
 
 
-def get_line_range(context, range = None):
+def lines_and_range(context, range = None):
     '''Get the range of the full lines containing the current (or supplied) range'''
     line_ending = get_line_ending(context)
     len_line_ending = len(line_ending)
@@ -122,7 +122,6 @@ def balance_line_endings(first, second, line_ending):
         if second[-len_line_ending:] == line_ending: second = second[:-len_line_ending]
     return first, second
 
-
 def insert_text_with_insertion_point(context, text, range):
     '''Extracts insertion point tokens, inserts the text, and selects the insertion point range'''
     text, ip_range = extract_insertion_point_range(text)
@@ -154,44 +153,87 @@ def extract_insertion_point_range(text):
             return text, new_range(start, end - start)
 
 
-def words_and_range(context, range=False):
+def words_and_range(context, range = None):
     '''Get text and range for the current word(s)'''
-    text = context.string()
-    line, line_range = line.lines_and_range(context)
     
-    if range == False:
+    # word characters, for our purposes. include < and > for html, $ for php variables.
+    word_chars = 'a-zA-Z0-9_\\?|`<>\$'
+    
+    line_ending = tea.get_line_ending(context)
+    text = context.string()
+    line_text, line_range = lines_and_range(context)
+    
+    if range is None:
         selection, selection_range = selection_and_range(context)
     else:
         selection_range = range
-        selection = get_selection(context)
-    
-    # move the start (out)
-    if selection_range.location > line_range.location and not selection.startswith(('\t', ' ')):
-        prefix = text[line_range.location:selection_range.location]
-        start = max(prefix.rfind(' '), prefix.rfind('\t'))
-        if start == -1:
-            start = line_range.location
-        else:
-            # plus one (at the end) is for the width of the whitespace character.
-            start += line_range.location + 1
-        selection_range = new_range(start, selection_range.length - selection_range.location + start)
         selection = get_selection(context, selection_range)
     
+    # move the start (out)
+    prefix_start = line_range.location
+    prefix_end = selection_range.location
+    if prefix_end > prefix_start and not re.match('[^%s]' % word_chars, selection):
+        result = rfind_not_chars(text[prefix_start:prefix_end], word_chars)
+        if result != -1:
+            start = prefix_start + result
+        else:
+            start = prefix_start
+        
+        selection_range = new_range(start, selection_range.length + prefix_end - start)
+        selection = get_selection(context, selection_range)
+
     suffix_start = selection_range.location + selection_range.length
     suffix_end = line_range.location + line_range.length
-    if suffix_start < suffix_end and not selection.endswith(('\t', ' ')):
-        suffix = text[suffix_start:suffix_end]
-        end = min(suffix.find(' '), suffix.rfind('\t'))
-        if end == -1:
-            end = suffix_end
+    if suffix_start < suffix_end and not re.search('[^%s]$' % word_chars, selection):
+        result = find_not_chars(text[suffix_start:suffix_end], word_chars)
+        if result != -1:
+            end = result + suffix_start
         else:
-            end += suffix_start
+            end = suffix_end
+        
         selection_range = new_range(selection_range.location, end - selection_range.location)
         selection = get_selection(context, selection_range)
     
+    tea.say(context, '', '||%s||' % selection)
     return selection, selection_range
 
 
 def beep():
     '''System beep!'''
     AppKit.NSBeep()
+
+
+def find_chars(text, chars):
+    '''Find the first index of any character from chars in text. chars is the guts of a regex []'''
+    result = re.search('[%s]' % chars, text)
+    if result:
+        return result.start()
+    else:
+        return -1
+
+
+def rfind_chars(text, chars):
+    '''Find the last index of any character from chars in text. chars is the guts of a regex []'''
+    result = re.search('([%s])[^%s]*$' % (chars, chars), text)
+    if result:
+        return result.end(1)
+    else:
+        return -1
+
+
+def find_not_chars(text, chars):
+    '''Find the first index of any character not in chars in text. chars is the guts of a regex []'''
+    result = re.search('[^%s]' % chars, text)
+    if result:
+        return result.start()
+    else:
+        return -1
+
+
+def rfind_not_chars(text, chars):
+    '''Find the last index of any character not in chars in text. chars is the guts of a regex []'''
+    result = re.search('([^%s])[%s]*$' % (chars, chars), text)
+    if result:
+        return result.end(1)
+    else:
+        return -1
